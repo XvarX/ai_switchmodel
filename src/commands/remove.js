ac8cfd414806f65d19adc36542c2510521b70bcc
XvarX <yexangjun@qq.com>
2026-05-09 12:10:17 +0800

    feat: cs remove command

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -1,3 +1,17 @@
-module.exports = function() {
-  console.log('Not yet implemented');
+const config = require('../config');
+
+module.exports = function(args) {
+  const name = args[0];
+  if (!name) {
+    console.error('  Usage: cs remove <name>');
+    process.exit(1);
+  }
+
+  if (!config.getProvider(name)) {
+    console.error(`  Provider "${name}" not found.`);
+    process.exit(1);
+  }
+
+  config.removeProvider(name);
+  console.log(`  ✓ Provider "${name}" removed.`);
 };
