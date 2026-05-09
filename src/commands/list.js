155dfa1d3456f703199ea82873b13294bf818070
XvarX <yexangjun@qq.com>
2026-05-09 12:03:11 +0800

    feat: cs list command

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,3 +1,18 @@
+const config = require('../config');
+
 module.exports = function() {
-  console.log('Not yet implemented');
+  const providers = config.listProviders();
+  const names = Object.keys(providers);
+
+  if (names.length === 0) {
+    console.log('  No providers configured. Run "cs add <name>" to add one.');
+    return;
+  }
+
+  console.log('  Providers:\n');
+  for (const name of names) {
+    const p = providers[name];
+    console.log(`    ${name}  →  ${p.url}  (${p.sonnet})`);
+  }
+  console.log();
 };
